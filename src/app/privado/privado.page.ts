fedf9a8f0df4082943db80b17155c6ae6a2699ec
Alex1399 <jhhoni00@gmail.com>
2023-12-10 10:51:26 -0500

    Cambios por Alex

--- src/app/privado/privado.page.ts
+++ src/app/privado/privado.page.ts
@@ -5,8 +5,8 @@ import banco from 'src/assets/banco/analisis.json';
 interface Pregunta {
   id: string;
   nombre: string;
-  balones?: { [key: string]: { balon: string } };
-  arrayBalones?: { balon: string }[];
+  balones?: { [key: string]: { balon: string; mesa?: string[] } };
+  arrayBalones?: { balon: string; mesa?: string[] }[];
 }
 
 @Component({
@@ -19,10 +19,15 @@ export class PrivadoPage implements OnInit {
   public arrayPregunta: Pregunta[] = [];
   public preguntaSeleccionadaId: string = '';
   public pregunta: Pregunta = { id: '', nombre: '' }; // Inicializado con valores por defecto
+  public opcionMesa: string = '';
 
   constructor(private alertController: AlertController) { } // Inyecta AlertController
 
   ngOnInit() {
+    this.cargarPreguntas();
+  }
+
+  cargarPreguntas() {
     for (const preguntaId in this.analisis) {
       if (this.analisis.hasOwnProperty(preguntaId)) {
         const pregunta: Pregunta = {
@@ -43,34 +48,37 @@ export class PrivadoPage implements OnInit {
   onPreguntaChange() {
     // Buscar la pregunta seleccionada en arrayPregunta por el id
     this.pregunta = this.arrayPregunta.find(item => item.id === this.preguntaSeleccionadaId) || { id: '', nombre: '' };
-
+    
     // Limpiar arrayBalones al cambiar la pregunta
     this.pregunta.arrayBalones = this.pregunta.arrayBalones || [];
-
+    
     // Rellenar arrayBalones con los balones de la pregunta seleccionada
     if (this.pregunta.balones) {
-      this.pregunta.arrayBalones = Object.values(this.pregunta.balones);
+    this.pregunta.arrayBalones = Object.values(this.pregunta.balones);
     }
-
+    
+    // Limpiar la selección de opciones de mesa
+    this.opcionMesa = '';
+    
     console.log('Pregunta seleccionada:', this.pregunta);
     console.log('Array de Balones:', this.pregunta ? this.pregunta.arrayBalones : []);
-  }
+    }
 
-  // Función para mostrar una alerta
   async mostrarMensaje(balon: string) {
-    const alert = await this.alertController.create({
-      header: 'Balón Seleccionado',
-      message: `Mensaje personalizado para ${balon}`,
-      buttons: ['OK']
-    });
+  const alert = await this.alertController.create({
+    header: 'Balón Seleccionado',
+    message: `Mensaje personalizado para ${balon} y la opción de mesa ${this.opcionMesa}`,
+    buttons: ['OK']
+  });
 
-    await alert.present();
+  await alert.present();
 
-    // Limpiar la pantalla después de mostrar el mensaje
-    this.preguntaSeleccionadaId = '';
-    this.pregunta = { id: '', nombre: '' };
-  }
+  this.limpiarPantalla();
 }
 
-
-
+  limpiarPantalla() {
+    this.preguntaSeleccionadaId = '';
+    this.pregunta = { id: '', nombre: '' };
+    this.opcionMesa = '';
+  }
+}
