bc818da2f26342ce9cad9d44f23c3d1b13064892
Alex1399 <jhhoni00@gmail.com>
2023-12-13 10:51:09 -0500

    Cambios Alex

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router'; // Importa Router
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-inicio',
